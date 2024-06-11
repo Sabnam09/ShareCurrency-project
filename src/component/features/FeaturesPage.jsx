c994fc9bdeebaabef4d6bd161fcba4300e6d3f69
Sabnam09 <151497611+Sabnam09@users.noreply.github.com>
2024-06-11 18:17:20 +0530

    ---

--- src/component/features/FeaturesPage.jsx
+++ src/component/features/FeaturesPage.jsx
@@ -72,67 +72,7 @@ function FeaturesPage() {
                     </div>
                 </div>
             </div>
-            <div className="row featuresec-row">
-                <div className="col-lg-5 featuresec-coltxt">
-                    <div>
-                        <h2>Know About Share Currency</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde necessitatibus, obcaecati esse ratione corrupti quae similique error sequi debitis sint inventore, odit, accusamus molestias magni a neque perspiciatis molestiae praesentium.</p>
-                    </div>
-                </div>
-                <div className="col-lg-7 featuresec-boxes">
-                    <section className="sec-1">
-                        <div className="row zigzag-row">
-                            <div className="col-6 zigzag-box offset-up">
-                                <div className="sec-1Box featurebox-animation">
-                                    <div className="dd">
-                                        <FaBriefcase />
-                                    </div>
-                                    <div className="sec-1text">
-                                        <h1>Our Approach</h1>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur a minima ullam.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-6 zigzag-box offset-down">
-                                <div className="sec-1Box featurebox-animation">
-                                    <div className="dd">
-                                        <FaBriefcase />
-                                    </div>
-                                    <div className="sec-1text">
-                                        <h1>Our Approach</h1>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur a minima ullam.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-6 zigzag-box offset-up">
-                                <div className="sec-1Box featurebox-animation">
-                                    <div className="dd">
-                                        <FaBriefcase />
-                                    </div>
-                                    <div className="sec-1text">
-                                        <h1>Our Approach</h1>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur a minima ullam.</p>
-                                    </div>
-                                </div>
-                                <br />
-                            </div>
-                            <div className="col-6 zigzag-box offset-down">
-                                <div className="sec-1Box featurebox-animation">
-                                    <div className="dd">
-                                        <FaBriefcase />
-                                    </div>
-                                    <div className="sec-1text">
-                                        <h1>Our Approach</h1>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur a minima ullam.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
-
-
+           
             <section className="sec-2">
                 <h1>Awesome Style</h1>
                 <p>Est At Euismod Ponderum Reformidans. Vivendo Imperdiet Usu Eu, Cum Ea Fabellas Adipisci, Suas Agam Ad Mei. Quo Mollis Euripidis Gloriatur Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero tempora quis incidunt at, velit quaerat repudiandae earum praesentium expedita laborum fuga quia adipisci magnam numquam nostrum cumque, sunt quam excepturi!.</p>
